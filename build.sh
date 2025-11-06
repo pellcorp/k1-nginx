20065195e062adad9292ea040aa7454f7ae0bb0f
Jason Pell <jason@pellcorp.com>
2025-11-06 21:15:33 +1100

    initial nginx build untested

---
+++ build.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+# in case build is executed from outside current dir be a gem and change the dir
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd -P)"
+cd $CURRENT_DIR
+
+if [ ! -f /.dockerenv ]; then
+  echo "ERROR: This script is only supported running in docker"
+  exit 1
+fi
+
+if [ -d build ]; then
+    rm -rf build/
+fi
+mkdir -p build
+cd build
+
+ver=1.28.0
+#curl -fLO https://nginx.org/download/nginx-1.28.0.tar.gz
+tar xzf ../nginx-${ver}.tar.gz
+
+# PCRE2 (used only for regex in config; disable JIT)
+#curl -fLO https://github.com/PCRE2Project/pcre2/releases/download/pcre2-10.43/pcre2-10.43.tar.gz
+tar xzf ../pcre2-10.43.tar.gz
+
+cd nginx-${ver}
+
+export TOOL=/opt/toolchains/mips-gcc720-glibc229
+export CROSS=mips-linux-gnu-
+export SYSROOT=/opt/k1-sysroot
+
+export CC="$TOOL/bin/${CROSS}gcc"
+export AR="$TOOL/bin/${CROSS}ar"
+export RANLIB="$TOOL/bin/${CROSS}ranlib"
+export STRIP="$TOOL/bin/${CROSS}strip"
+
+export CFLAGS="--sysroot=$SYSROOT -Os -pipe -EL -march=mips32r2 -mhard-float -mfp64 -mnan=2008 -mno-mips16 -mno-micromips -fno-strict-aliasing -D_LARGEFILE_SOURCE -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64"
+export LDFLAGS="--sysroot=$SYSROOT -Wl,-EL -Wl,-m,elf32ltsmip -Wl,--gc-sections -Wl,-rpath-link,$SYSROOT/lib -Wl,-rpath-link,$SYSROOT/usr/lib -Wl,--dynamic-linker=/lib/ld-linux-mipsn8.so.1 -static-libgcc"
+
+export QEMU_LD_PREFIX=/opt/k1-sysroot
+
+./configure \
+  --with-cc="$CC" \
+  --with-cc-opt="$CFLAGS" \
+  --with-ld-opt="$LDFLAGS" \
+  --crossbuild=Linux::mipsel \
+  --prefix=/usr \
+  --conf-path=/etc/nginx/nginx.conf \
+  --sbin-path=/usr/sbin/nginx \
+  --pid-path=/var/run/nginx.pid \
+  --lock-path=/var/run/lock/nginx.lock \
+  --user=www-data --group=www-data \
+  --error-log-path=/var/log/nginx/error.log \
+  --http-log-path=/var/log/nginx/access.log \
+  --http-client-body-temp-path=/var/tmp/nginx/client-body \
+  --http-proxy-temp-path=/var/tmp/nginx/proxy \
+  --http-fastcgi-temp-path=/var/tmp/nginx/fastcgi \
+  --http-scgi-temp-path=/var/tmp/nginx/scgi \
+  --http-uwsgi-temp-path=/var/tmp/nginx/uwsgi \
+  --with-pcre=../pcre2-10.43 \
+  --without-pcre2 \
+  --without-http_gzip_module \
+  --without-http-cache \
+  --without-http_fastcgi_module \
+  --without-http_uwsgi_module \
+  --without-http_scgi_module
+
+make -j"$(nproc)"
+make DESTDIR="$PWD/_staging" install
+$STRIP _staging/usr/sbin/nginx || true
+
+echo "Creating tarball..."
+tar -C _staging -czf $CURRENT_DIR/build/nginx.tar.gz .
